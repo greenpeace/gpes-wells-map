3fb41b4b263f23317bbcc2185216dacc859d6709
Osvaldo Gago <opgago@yahoo.com>
2019-02-26 18:08:33 +0100

    Coger datos del Json

--- _includes/media-1/scripts.js
+++ _includes/media-1/scripts.js
@@ -6,18 +6,22 @@
     }).setView([37.2141193, -4.3873866], 6.5);
 
     L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png?lang=es', {
-        maxZoom: 8,
+        maxZoom: 9,
         minZoom: 6.5,
         attribution: '&copy; OpenStreetMap contributors, Wikimedia',
         id: 'mapbox.light'
     }).addTo(osmap);
-     
-	L.circle([37.2141193, -4.3873866], 15000, {
-		// color: '#f00',
-        stroke: false,
-		fillColor: '#008',
-		fillOpacity: 0.7
-	}).addTo(osmap).bindPopup("<h4>Mensage en el popup</h4><p>Cantidad: Valor</p>");
+    
+          
+    guadalquivir.forEach(function(node){
+        L.circle([node.lat, node.long], (node.NumSanciones * 100 ) + 5000, {
+            // color: '#f00',
+            stroke: false,
+            fillColor: '#800',
+            fillOpacity: 0.6
+        }).addTo(osmap).bindPopup("<h4>" + node.Municipio + "</h4><p>Número de sanciones: " + node.NumSanciones + "</p>");        
+    });
+
 
     /* END SCRIPTS */
 });
