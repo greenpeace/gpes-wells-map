4559ff5d94811933f4ee54e0ae7d6ead7b2baf46
Osvaldo Gago <opgago@yahoo.com>
2019-03-06 21:45:13 +0100

    Todos los otros ficheros

---
+++ es/media-4.js
@@ -0,0 +1,12 @@
+---
+
+# File with the language elements, inside the _data folder. To translate please duplicate the file original.yml
+lang_file: es-ES
+
+---
+
+var guadalquivir_sanciones2 = {% include media-4/guadalquivir-numsanciones-extraccion.json %};
+
+var guadalquivir_danos2 = {% include media-4/guadalquivir-danos-extraccion.json %};
+
+{% include media-4/scripts.js %}
